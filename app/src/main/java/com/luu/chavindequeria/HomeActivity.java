30fa44ce82408f8c1ffb3aa7e009f6ccc76cb6dc
lnaranj0 <lagos_2_5@hotmail.com>
2018-06-20 18:09:03 -0500

    Menú lateral

---
+++ app/src/main/java/com/luu/chavindequeria/HomeActivity.java
@@ -0,0 +1,184 @@
+package com.luu.chavindequeria;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+
+import com.luu.chavindequeria.utils.CrossfadeWrapper;
+import com.mikepenz.crossfader.Crossfader;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.MiniDrawer;
+import com.mikepenz.materialdrawer.holder.BadgeStyle;
+import com.mikepenz.materialdrawer.interfaces.ICrossfader;
+import com.mikepenz.materialdrawer.interfaces.OnCheckedChangeListener;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.SwitchDrawerItem;
+import com.mikepenz.materialdrawer.model.ToggleDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialize.util.SystemUtils;
+import com.mikepenz.materialize.util.UIUtils;
+
+public class HomeActivity extends AppCompatActivity {
+    private static final int PROFILE_SETTING = 1;
+
+    //Save our header or result
+    private AccountHeader headerResult = null;
+    private Drawer result = null;
+    private MiniDrawer miniResult = null;
+    private Crossfader crossFader;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Handle Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.nombreBarra);
+
+        final IProfile profile = new ProfileDrawerItem().withName("Luu Naranjo").withEmail("lagos_2_5@hotmail.com").withIcon(R.drawable.lu_nuevo);
+
+
+        //Create the account header
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .withTranslucentStatusBar(false)
+                .addProfiles(
+                        profile
+                ).withSavedInstance(savedInstanceState)
+                .build();
+
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withTranslucentStatusBar(false)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Agregar Pedido").withIcon(R.drawable.food).withIdentifier(1),
+                        new PrimaryDrawerItem().withName("Ver Pedidos").withIcon(R.drawable.tv).withBadgeStyle(new BadgeStyle(Color.RED, Color.RED)).withIdentifier(2).withSelectable(false),
+                        new PrimaryDrawerItem().withName("Cuenta").withIcon(R.drawable.payment).withIdentifier(3),
+                        new PrimaryDrawerItem().withName("Clientes").withIcon(R.drawable.man_user).withIdentifier(4),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName("Ajustes").withIcon(R.drawable.settings).withIdentifier(5)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if(drawerItem instanceof Nameable){
+                            Toast.makeText(getApplicationContext(),((Nameable) drawerItem).getName().getText(HomeActivity.this),Toast.LENGTH_LONG).show();
+                        }
+                        return false;
+                    }
+                })
+                .withGenerateMiniDrawer(true)
+                .withSavedInstance(savedInstanceState)
+                .buildView();
+
+        miniResult = result.getMiniDrawer();
+
+        int firstWidth = (int) UIUtils.convertDpToPixel(300, this);
+        int secondWidth = (int) UIUtils.convertDpToPixel(72,this);
+
+
+        crossFader = new Crossfader()
+                .withContent(findViewById(R.id.crossfade_content))
+                .withFirst(result.getSlider(), firstWidth)
+                .withSecond(miniResult.build(this),secondWidth)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        //Define the crossfader to be used with the miniDrawer. this is required to be able to automatically toggle open(close
+        miniResult.withCrossFader(new CrossfadeWrapper(crossFader));
+
+        //Define a shadow
+        crossFader.getCrossFadeSlidingPaneLayout().setShadowResourceLeft(R.drawable.material_drawer_shadow_left);
+
+    }
+
+
+    private OnCheckedChangeListener onCheckedChangeListener = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(IDrawerItem drawerItem, CompoundButton buttonView, boolean isChecked) {
+            if (drawerItem instanceof  Nameable){
+                Log.i("material-drawer", "DrawerItem: " + ((Nameable) drawerItem).getName() + " - toggleChecked: " + isChecked);
+            }else {
+                Log.i("material-drawer", "toggleChecked: " + isChecked);
+            }
+        }
+    };
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        outState = result.saveInstanceState(outState);
+
+        outState = headerResult.saveInstanceState(outState);
+
+        outState = crossFader.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        if (SystemUtils.getScreenOrientation() == Configuration.ORIENTATION_LANDSCAPE) {
+            inflater.inflate(R.menu.embedded, menu);
+            menu.findItem(R.id.menu_1).setIcon(R.drawable.settings);
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (crossFader != null && crossFader.isCrossFaded()) {
+            crossFader.crossFade();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle the click on the back arrow click
+        switch (item.getItemId()) {
+            case R.id.menu_1:
+                crossFader.crossFade();
+                return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/luu/chavindequeria/MainActivity.java
+++ app/src/main/java/com/luu/chavindequeria/MainActivity.java
@@ -1,5 +1,7 @@
 package com.luu.chavindequeria;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Verificar la configuración y bases de datos, así como el login
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(intent);
+                finish();
+                }
+            },3000);
+
+
     }
 }
